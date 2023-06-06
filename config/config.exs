c4853a9f3e7b5affe8b20add9e0909037b4fce1b
Roman <godmaire@twilit.io>
2023-06-06 17:01:17 -0400

    Change log level in production

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,7 @@ import Config
 
 config :nostrum,
   youtubedl: "/usr/bin/yt-dlp"
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
